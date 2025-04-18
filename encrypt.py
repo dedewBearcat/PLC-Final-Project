2c72ed26f8e15f2ca528a7d25f11d7862f0f5742
Andrew <81774507+dedewBearcat@users.noreply.github.com>
2025-04-18 13:19:38 -0400

    Update encrypt.py
    comments

--- encrypt.py
+++ encrypt.py
@@ -4,26 +4,26 @@ import datetime
 def __encrypt(path, seeder = None):
     datetimer = str(datetime.datetime.now())
     codedmessage = ""
-    if seeder is None:
+    if seeder is None: #if no seed is given, use the datetime to call keyGen
         key = keyGen.keyGen(datetimer)
-    else:
+    else: #else calls keyGen using the user given seeder
         key = keyGen.keyGen(seeder)
     file = open(path, "r")
-    for line in file:
+    for line in file: #rewrites character by character from file to codedmessage string var
         uncodedmessage = line.strip()
         for char in uncodedmessage:
             for sub in key:
                 if char == sub[0]:
                     codedmessage += sub[1]
         codedmessage += "\n"
-    try:
+    try: #writes codedmessage into the new text file
         filename = path.replace(".txt", "")
         with open(filename + "_coded.txt", "x") as output:
             output.write(datetimer)
             output.write("\n")
             output.write(codedmessage)
-            result = f"Message has been successfully coded. Look for {filename}_coded.txt."
+            result = f"Message has been successfully coded. Look for {filename}_coded.txt." #results to print
     except FileExistsError:
-        result = f"File by {filename}_coded.txt already exists. Encoding process cancelled."
+        result = f"File by {filename}_coded.txt already exists. Encoding process cancelled." #results to print
     print(result)
     return result
