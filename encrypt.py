e136638bdb1033dfeae5f736312a86d1b7bbe51d
Andrew <81774507+dedewBearcat@users.noreply.github.com>
2025-04-18 12:45:17 -0400

    Update encrypt.py
    final draft - filename changed to path, return result of function added

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,14 @@
 import keyGen
 import datetime
 
-def __encrypt(filename, seeder = None):
+def __encrypt(path, seeder = None):
     datetimer = str(datetime.datetime.now())
-    file = open(filename, "r")
     codedmessage = ""
     if seeder is None:
         key = keyGen.keyGen(datetimer)
     else:
         key = keyGen.keyGen(seeder)
+    file = open(path, "r")
     for line in file:
         uncodedmessage = line.strip()
         for char in uncodedmessage:
@@ -17,13 +17,13 @@ def __encrypt(filename, seeder = None):
                     codedmessage += sub[1]
         codedmessage += "\n"
     try:
-        filename = filename.replace(".txt", "")
+        filename = path.replace(".txt", "")
         with open(filename + "_coded.txt", "x") as output:
             output.write(datetimer)
             output.write("\n")
             output.write(codedmessage)
-            print(f"Message has been successfully coded. Look for {filename}_coded.txt.")
+            result = f"Message has been successfully coded. Look for {filename}_coded.txt."
     except FileExistsError:
-        print(f"File by {filename}_coded.txt already exists. Encoding process cancelled.")
-
-__encrypt("testmessage1.txt", "Isnt this string")
+        result = f"File by {filename}_coded.txt already exists. Encoding process cancelled."
+    print(result)
+    return result
