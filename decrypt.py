1f01a155405ad7f74fe0010d432da09b4040ff8c
Andrew <81774507+dedewBearcat@users.noreply.github.com>
2025-04-18 13:19:21 -0400

    Update decrypt.py
    comments

--- decrypt.py
+++ decrypt.py
@@ -3,25 +3,25 @@ import keyGen
 def __decrypt(path, seeder = None):
     file = open(path, "r")
     uncodedmessage = ""
-    if seeder is None:
+    if seeder is None: #if no seed is given, use the datetime which is always on the first line
         seeder = file.readline()
         seeder = seeder.strip()
-    else:
+    else: #since datetime is always on the first line, this gets rid of the first line so it doesn't go throught he decoding process
         file.readline()
-    key = keyGen.keyGen(seeder)
-    for line in file:
+    key = keyGen.keyGen(seeder) #calls on keyGen using the given seed or using the datetime
+    for line in file: #rewrites character by character from file to codedmessage string var
         codedmessage = line.strip()
         for char in codedmessage:
             for sub in key:
                 if char == sub[1]:
                     uncodedmessage += sub[0]
         uncodedmessage += "\n"
-    try:
+    try:#writes decodedmessage into the new text file
         newfilename = path.replace(".txt", "")
         with open(newfilename + "_decoded.txt", "x") as output:
             output.write(uncodedmessage)
-            result = f"Message has been successfully decoded. Look for {newfilename}_decoded.txt."
+            result = f"Message has been successfully decoded. Look for {newfilename}_decoded.txt." #results to print
     except FileExistsError:
-        result = f"File by {newfilename}_decoded.txt already exists. Decoding process cancelled."
+        result = f"File by {newfilename}_decoded.txt already exists. Decoding process cancelled." #results to print
     print(result)
     return result
